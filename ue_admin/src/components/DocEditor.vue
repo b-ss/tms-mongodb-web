7c5428783812f248f43dbc49a8284d533deabd49
b-ss <2764411477@qq.com>
2020-11-05 22:35:55 +0800

    modify admin doc

--- ue_admin/src/components/DocEditor.vue
+++ ue_admin/src/components/DocEditor.vue
@@ -1,12 +1,24 @@
 <template>
-  <el-dialog :visible.sync="dialogVisible" :destroy-on-close="destroyOnClose" :close-on-click-modal="closeOnClickModal">
-    <tms-el-json-doc :is-submit="isSubmit" :schema="body" :doc="document" v-on:submit="onJsonDocSubmit" :on-file-submit="handleFileSubmit" :on-axios="handleAxios" :on-file-download="handleDownload"></tms-el-json-doc>
+  <el-dialog
+    :visible.sync="dialogVisible"
+    :destroy-on-close="destroyOnClose"
+    :close-on-click-modal="closeOnClickModal"
+  >
+    <tms-el-json-doc
+      :is-submit="isSubmit"
+      :schema="body"
+      :doc="document"
+      v-on:submit="onJsonDocSubmit"
+      :on-file-submit="handleFileSubmit"
+      :on-axios="handleAxios"
+      :on-file-download="handleDownload"
+    ></tms-el-json-doc>
   </el-dialog>
 </template>
 <script>
 import { ElJsonDoc as TmsElJsonDoc } from 'tms-vue-ui'
 import { TmsAxios } from 'tms-vue'
-import { priceValidate as onValidate, priceFormat as onFormat, randomUUID } from '../tms/utils.js'
+import utils from '../src/tms/utils'
 import apiDoc from '../apis/document'
 import apiSchema from '../apis/schema'
 
@@ -26,36 +38,45 @@ export default {
       collection: null,
       destroyOnClose: true,
       closeOnClickModal: false,
+      plugins: []
     }
   },
   methods: {
-    handleAxios() {    
+    handleAxios() {
       return TmsAxios.ins('mongodb-api')
     },
     handleDownload(name, url) {
       const access_token = sessionStorage.getItem('access_token')
-      window.open(`${process.env.VUE_APP_BACK_API_FS}${url}?access_token=${access_token}`)
+      window.open(
+        `${process.env.VUE_APP_BACK_API_FS}${url}?access_token=${access_token}`
+      )
     },
     handleFileSubmit(ref, files) {
       let result = {}
       let objPromises = files.map(file => {
         if (file.hasOwnProperty('url')) {
-          return {'name': file.name, 'url': file.url}
+          return { name: file.name, url: file.url }
         }
         const fileData = new FormData()
         fileData.append('file', file)
         const config = { 'Content-Type': 'multipart/form-data' }
-        const dirPath = this.dbName + '/' + this.collection.name + '/' + this.document.order_id + '/' + ref
-        return apiDoc.upload(
-          { bucket: this.bucketName, dir: dirPath }, fileData, config
-          )
+        const dirPath =
+          this.dbName +
+          '/' +
+          this.collection.name +
+          '/' +
+          this.document.order_id +
+          '/' +
+          ref
+        return apiDoc
+          .upload({ bucket: this.bucketName, dir: dirPath }, fileData, config)
           .then(path => {
-            return Promise.resolve({'url': path, 'name': file.name})
+            return Promise.resolve({ url: path, name: file.name })
           })
           .catch(err => Promise.reject(err))
       })
       return Promise.all(objPromises)
-        .then(rsl => { 
+        .then(rsl => {
           result[ref] = rsl
           return Promise.resolve(result)
         })
@@ -63,19 +84,17 @@ export default {
     },
     onJsonDocSubmit(slimDoc, newDoc) {
       this.isSubmit = true
-      let validate = true
-      if (newDoc.source && newDoc.source==='3' && !newDoc.order_id) {
-        newDoc.order_id = randomUUID()
-      }
-      if (process.env.VUE_APP_SUBMIT_VALITOR_FIELD) {
-        const config = process.env.VUE_APP_SUBMIT_VALITOR_FIELD	
-        validate =  Object.entries(newDoc).map(([key, value]) => {
-          if (config.indexOf(key)===-1) return true
-          const flag = onValidate(this.collection.schema.body.properties, key, value)
-          if (flag) newDoc[key] = onFormat(value)
-          return flag
-        }).every(ele => ele === true)
-      }
+      let validate = this.plugins
+        .map(item => {
+          const result = utils[item](this.body, newDoc)
+          if (result.msg === 'success') {
+            newDoc = result.data
+            return true
+          } else {
+            return false
+          }
+        })
+        .every(ele => ele === true)
       if (!validate) {
         this.isSubmit = false
         return false
@@ -90,26 +109,28 @@ export default {
             newDoc
           )
           .then(newDoc => this.$emit('submit', newDoc))
-          .finally(() => this.isSubmit = false)
+          .finally(() => (this.isSubmit = false))
       } else {
         apiDoc
           .create(this.bucketName, this.dbName, this.collection.name, newDoc)
           .then(newDoc => this.$emit('submit', newDoc))
-          .finally(() => this.isSubmit = false)
+          .finally(() => (this.isSubmit = false))
       }
     },
     async handleProperty() {
-      let tags = (process.env.VUE_APP_TAGS && process.env.VUE_APP_TAGS.split(',')) || this.collection.tags
+      let tags =
+        (process.env.VUE_APP_TAGS && process.env.VUE_APP_TAGS.split(',')) ||
+        this.collection.tags
       let body = {}
       if (tags && tags.length) {
-        for(let i=0; i<tags.length; i++) {
+        for (let i = 0; i < tags.length; i++) {
           let schemas = await apiSchema.listByTag(this.bucketName, tags[i])
           schemas.forEach(schema => {
             Object.entries(schema.body).forEach(([key, val]) => {
               if (val && typeof val === 'object') {
                 // 如果属性值为空就不合并
                 if (!body[key]) body[key] = {}
-                if (JSON.stringify(val)!=='{}') Object.assign(body[key], val)
+                if (JSON.stringify(val) !== '{}') Object.assign(body[key], val)
               } else {
                 body[key] = val
               }
@@ -125,9 +146,22 @@ export default {
       this.bucketName = bucketName
       this.dbName = dbName
       this.collection = collection
+      if (process.env.VUE_APP_FRONT_DOCEDITOR_ADD) {
+        let str = process.env.VUE_APP_FRONT_DOCEDITOR_ADD.replace(/\s/g, '')
+        this.plugins = str.split(',')
+      }
       await this.handleProperty()
-      if (doc && doc._id)  {
-        this.document = JSON.parse(JSON.stringify(Object.assign(this.document, doc)))
+      if (doc && doc._id) {
+        if (process.env.VUE_APP_FRONT_DOCEDITOR_MODIFY) {
+          let str = process.env.VUE_APP_FRONT_DOCEDITOR_MODIFY.replace(
+            /\s/g,
+            ''
+          )
+          this.plugins = str.split(',')
+        }
+        this.document = JSON.parse(
+          JSON.stringify(Object.assign(this.document, doc))
+        )
       }
       this.$mount()
       document.body.appendChild(this.$el)
--- ue_admin/src/tms/utils.js
+++ ue_admin/src/tms/utils.js
@@ -1,52 +1,120 @@
 import { Message } from 'element-ui'
 import { v5 as uuidv5 } from 'uuid'
 
-function priceValidate(schema, key, val) {
-  if (isNaN(Number(val))) {
-    Message.error({ message: schema[key].title + '应填入正确的数字', customClass: 'mzindex' })
-    return false
+export default class Utils {
+  constructor() {}
+  /**
+   * 生成随机数
+   */
+  static getRandom(numbers = 32) {
+    const timestamp = new Date().getTime()
+    const namespace = '1b671a64-40d5-491e-99b0-da01ff1f3341'
+    const md = uuidv5(timestamp.toString(), namespace)
+    let result = md.replace(/-/g, '').substr(0, numbers)
+    return result
   }
-  if (Number(val) < 0) {
-    Message.error({ message: schema[key].title + '的值不能小于0', customClass: 'mzindex' })
-    return false
+  /**
+   * 生成订单编号
+   */
+  static generateOrderId(schema, doc) {
+    if (!schema.properties.source && !schema.properties.order_id) {
+      return { msg: 'success', data: doc }
+    }
+    if (doc.source && doc.source === '3') {
+      doc['order_id'] = Utils.getRandom()
+    }
+    return { msg: 'success', data: doc }
   }
-  const value = val.split('.')
-  if (value.length > 2) {
-    Message.error({ message: schema[key].title + '格式错误', customClass: 'mzindex' })
-    return false
+  /**
+   * 定义审核状态
+   */
+  static defineAuditStatus(schema, doc) {
+    if (!schema.properties.auditing_status) {
+      return { msg: 'success', data: doc }
+    }
+    doc.auditing_status = '3'
+    return { msg: 'success', data: doc }
   }
-  if (value.length !== 1) {
-    const float = value[1]
-    if (float.length > 3) {
-      Message.error({ message: this.schema[key].title + '格式错误,小数点后不能大于3位', customClass: 'mzindex' })
+  /**
+   * 价格校验
+   */
+  static priceValidate(schema, key, val) {
+    if (isNaN(Number(val))) {
+      Message.error({
+        message: schema[key].title + '应填入正确的数字',
+        customClass: 'mzindex'
+      })
       return false
     }
-  }
-  return true
-}
-
-function priceFormat(value) {
-  if (value === "") return val
-  let val = String(Number(value))
-  let arrOfVal = val.split('.')
-  if (arrOfVal.length === 1) {
-    val = val + '.00'
-  } else {
-    let floatVal = arrOfVal[1].split('')
-    if (floatVal.length === 1) {
-      val = val + '0'
-    } else if (floatVal.length === 3 && floatVal[2] === '0') {
-      val = val.substr(0, val.length - 1)
+    if (Number(val) < 0) {
+      Message.error({
+        message: schema[key].title + '的值不能小于0',
+        customClass: 'mzindex'
+      })
+      return false
     }
+    const value = val.split('.')
+    if (value.length > 2) {
+      Message.error({
+        message: schema[key].title + '格式错误',
+        customClass: 'mzindex'
+      })
+      return false
+    }
+    if (value.length !== 1) {
+      const float = value[1]
+      if (float.length > 3) {
+        Message.error({
+          message: this.schema[key].title + '格式错误,小数点后不能大于3位',
+          customClass: 'mzindex'
+        })
+        return false
+      }
+    }
+    return true
   }
-  return val
-}
+  /**
+   * 价格格式化
+   */
+  static priceFormat(value) {
+    if (value === '') return value
+    let val = String(Number(value))
+    let arrOfVal = val.split('.')
+    if (arrOfVal.length === 1) {
+      val = val + '.00'
+    } else {
+      let floatVal = arrOfVal[1].split('')
+      if (floatVal.length === 1) {
+        val = val + '0'
+      } else if (floatVal.length === 3 && floatVal[2] === '0') {
+        val = val.substr(0, val.length - 1)
+      }
+    }
+    return val
+  }
+  /**
+   * 价格字段补零
+   */
+  static fillingZero(schema, doc) {
+    if (!process.env.VUE_APP_FILLINGZERO_FIELD)
+      return { msg: 'success', data: doc }
+    let str = process.env.VUE_APP_FILLINGZERO_FIELD.replace(/\s/g, '')
+    const config = str.split(',')
 
-function randomUUID() {
-  const name = new Date().getTime()
-  const namespace = '1b671a64-40d5-491e-99b0-da01ff1f3341'
-  const result = uuidv5(name.toString(), namespace)
-  return result.replace(/-/g, '')
-}
+    let validate = Object.entries(doc)
+      .map(([key, value]) => {
+        if (config.indexOf(key) === -1) return true
 
-export { priceValidate, priceFormat, randomUUID }
+        const flag = Utils.priceValidate(schema.properties, key, value)
+        if (flag) doc[key] = Utils.priceFormat(value)
+
+        return flag
+      })
+      .every(ele => ele === true)
+
+    let result = validate
+      ? { msg: 'success', data: doc }
+      : { msg: 'fail', data: null }
+    return result
+  }
+}
